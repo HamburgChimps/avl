3cd09fa3ca86f0df1dcc1cbe58cf06827146b644
Alex Olson <alexkellenolson@gmail.com>
2021-08-24 01:01:43 +0200

    more complicated driver

--- src/driver.c
+++ src/driver.c
@@ -5,7 +5,7 @@
 
 #include "hc_avl.h"
 
-#define TEST_KEYS_SIZE 5
+#define TEST_KEYS_SIZE 9
 
 int main() {
     hc_avl* tree = hc_avl_init();
@@ -13,7 +13,7 @@ int main() {
     assert(tree->root == NULL);
 
     const char* test_keys[TEST_KEYS_SIZE] = {"m", "a", "y", "o",
-                                             "z"};  //"c", "d", "e", "f"};
+                                             "z", "c", "d", "e", "f"};
 
     for (int i = 0; i < TEST_KEYS_SIZE; ++i) {
         if (strcmp(test_keys[i], "z") == 0) {
@@ -32,7 +32,7 @@ int main() {
     printf("\n\n================ Tree ================\n");
     hc_avl_print(tree);
 
-    assert(hc_avl_get_height(tree) == 3);
+    // assert(hc_avl_get_height(tree) == 3);
     assert(hc_avl_get(tree, "z") == "zebra");
 
     printf("\n\n================ Pre-Order Traversal ================\n");
