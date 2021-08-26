e4aef1f438bbb0e0d6a8be5f48e77447e074acfd
Alex Olson <alexkellenolson@gmail.com>
2021-08-27 00:14:31 +0200

    [wip] [broken] try to understand what is going on

--- src/driver.c
+++ src/driver.c
@@ -5,34 +5,22 @@
 
 #include "hc_avl.h"
 
-#define TEST_KEYS_SIZE 9
+#define TEST_KEYS_SIZE 6
 
 int main() {
     hc_avl* tree = hc_avl_init();
 
     assert(tree->root == NULL);
 
-    const char* test_keys[TEST_KEYS_SIZE] = {"m", "a", "y", "o", "z",
-                                             "c", "d", "e", "f"};
+    const char* test_keys[TEST_KEYS_SIZE] = {"a", "b", "c", "d", "e", "z"};
 
     for (int i = 0; i < TEST_KEYS_SIZE; ++i) {
-        if (strcmp(test_keys[i], "z") == 0) {
-            hc_avl_insert(tree, test_keys[i], "zebra");
-            continue;
-        }
-
-        if (strcmp(test_keys[i], "y") == 0) {
-            hc_avl_insert(tree, test_keys[i], "horse");
-            continue;
-        }
-
         hc_avl_insert(tree, test_keys[i], NULL);
     }
 
     printf("\n\n================ Tree ================\n");
     hc_avl_print(tree);
     // assert(hc_avl_get_height(tree) == 3);
-    assert(hc_avl_get(tree, "z") == "zebra");
     /*
         printf("\n\n================ Pre-Order Traversal ================\n");
         hc_avl_traverse(tree, -1);
@@ -48,7 +36,6 @@ int main() {
 
         */
 
-    hc_avl_delete_key(tree, "y");
     printf("\n\n================ Final State of Tree ================\n");
     hc_avl_print(tree);
     hc_avl_destroy(&tree);
